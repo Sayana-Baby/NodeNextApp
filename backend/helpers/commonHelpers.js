f118bca306c25800f9364ace32cc4f22d0000034
sayana baby <sayana.baby@ivldsp.com>
2025-07-30 21:05:00 +0530

    removing node code

--- backend/helpers/commonHelpers.js
+++
@@ -1,5 +0,0 @@
-
-
-exports.genResponse = (success=false,message='',data=null)=>{
-  return {success,message,data}
-}
--- backend/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const userRouter = express.Router();
-
-const auth = require('../controllers/user.controller');
-userRouter.post('/register',auth.register);
-userRouter.post('/login',auth.login);
-
-module.exports = userRouter;
